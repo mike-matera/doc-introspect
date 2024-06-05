336bb768b94f0f796c59e60431e6181d0dc26aae
Mike Matera <michael.matera@cabrillo.edu>
2024-06-05 11:52:26 -0700

    Fixes and some niceties.

--- doctags/__init__.py
+++ doctags/__init__.py
@@ -1,46 +1,60 @@
 """
-A simple IPython plugin that analyzes and remembers the contents 
-of cell docstrings so that the cell contents can be gotten in Python code. 
+A simple IPython plugin that analyzes and remembers the contents
+of cell docstrings so that the cell contents can be gotten in Python code.
 """
 
-import ast 
+import ast
 
 from IPython.core.getipython import get_ipython
 
 from . import cellcache
 
+
 def get(expr):
     """Get cache entry"""
     return cache.find(expr)
 
+
 def source(expr):
     return get(expr).source
+
 
 def tree(expr):
     return get(expr).tree
 
+
 def walk(expr):
     return ast.walk(tree(expr))
+
 
 def tokens(expr):
     return (x.__class__ for x in walk(expr))
 
+
 def result(expr):
     return get(expr).result
 
+
 def run(expr):
-    return get_ipython() \
-        .run_cell(source(expr), store_history=False, silent=False).result
+    return (
+        get_ipython()
+        .run_cell(source(expr), store_history=False, silent=False)
+        .result
+    )
+
 
 def load_ipython_extension(ipython):
     global cache
     cache = cellcache.CellCache()
-    ipython.events.register('post_run_cell', cache.post_run_cell)
+    ipython.run_cell("import doctags")
+    ipython.events.register("post_run_cell", cache.post_run_cell)
+
 
 def unload_ipython_extension(ipython):
     global cache
     cache = None
-    ipython.events.unregister('post_run_cell', cache.post_run_cell)
+    ipython.events.unregister("post_run_cell", cache.post_run_cell)
+
 
 # global singleton
-cache = None 
+cache = cellcache.CellCache()
--- doctags/cellcache.py
+++ doctags/cellcache.py
@@ -1,15 +1,16 @@
 """
-A class that remembers executed cells. 
+A class that remembers executed cells.
 """
 
-import re 
-import ast 
+import ast
+import re
 
 from IPython.core.getipython import get_ipython
 
+
 class CacheEntry:
     """
-    Information about an executed cell. 
+    Information about an executed cell.
     """
 
     def __init__(self, result):
@@ -24,14 +25,19 @@ class CacheEntry:
             self.tree = ast.parse(self.source)
             self.docstring = ast.get_docstring(self.tree)
 
-        except SyntaxError as e:
+        except SyntaxError:
             self.tree = None
             self.docstring = None
 
         if self.docstring is not None:
-            self.tags = [m.group(1) for x in self.docstring.split('\n') if (m := re.match('(@\S+)', x.strip())) is not None]
+            self.tags = [
+                m.group(1)
+                for x in self.docstring.split("\n")
+                if (m := re.match(r"(@\S+)", x.strip())) is not None
+            ]
         else:
             self.tags = []
+
 
 class CellCache:
     """
@@ -40,23 +46,21 @@ class CellCache:
 
     def __init__(self):
         self._cache = {
-            'by-id': {},
-            'by-tag': {},
-        } 
-    
+            "by-id": {},
+            "by-tag": {},
+        }
+
     def find(self, search):
         """Find a cache entry."""
-        if search.startswith('@'):
+        if search.startswith("@"):
             # find tag
-            return self._cache['by-tag'][search]
+            return self._cache["by-tag"][search]
         else:
             # find ID
-            return self._cache['by-id'][search]
-        
-    def post_run_cell(self, result):
-        entry = CacheEntry(result)        
-        self._cache['by-id'][entry.id] = entry
-        for tag in entry.tags:
-            self._cache['by-tag'][tag] = entry
+            return self._cache["by-id"][search]
 
-        
+    def post_run_cell(self, result):
+        entry = CacheEntry(result)
+        self._cache["by-id"][entry.id] = entry
+        for tag in entry.tags:
+            self._cache["by-tag"][tag] = entry
